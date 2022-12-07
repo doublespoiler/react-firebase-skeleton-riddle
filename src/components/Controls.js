3955e1a65f81e9d18dd4fb62aaf1a2261f087157
Skylan Lew <skylanslew@gmail.com>
2022-12-07 11:39:25 -0700

    add onclick
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,25 +1,30 @@
+function Controls(props){
 
+  const { answers, connectedRooms, correctAnswer, onClick} = props;
 
-function Controls(){
+  console.log(answers);
+  console.log(connectedRooms);
+  console.log(correctAnswer);
+
   return(
     <div className="player--controls">
-      <table>
-        <tr>
-          <td></td>
-          <td><button className="player--north">North</button></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td><button className="player--west">West</button></td>
-          <td></td>
-          <td><button className="player--east">East</button></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td><button className="player--south">South</button></td>
-          <td></td>
-        </tr>
-      </table>
+        <table>
+          <tr>
+            <td></td>
+            <td><button className="player--north" onClick={() => onClick(answers[2])}>{answers[2]} </button></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td><button className="player--west" onClick={() => onClick(answers[0])}>{answers[0]}</button></td>
+            <td></td>
+            <td><button className="player--east" onClick={() => onClick(answers[1])}>{answers[1]}</button></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td><button className="player--south" onClick={() => onClick(answers[3])}>{answers[3]}</button></td>
+            <td></td>
+          </tr>
+        </table>
     </div>
   )
 }
